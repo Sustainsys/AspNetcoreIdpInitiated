68db1b39bc98f0065900d72aaae6d4a96ef8307b
Anders Abel <anders.abel@sustainsys.com>
2020-06-23 22:22:05 +0200

    Use custom Acs query string param as returnUrl

--- AspNetCoreIdpInitiated/Startup.cs
+++ AspNetCoreIdpInitiated/Startup.cs
@@ -5,12 +5,15 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Sustainsys.Saml2;
 using Sustainsys.Saml2.Metadata;
+using Sustainsys.Saml2.Saml2P;
+using Sustainsys.Saml2.WebSso;
 
 namespace AspNetCoreIdpInitiated
 {
@@ -22,6 +25,8 @@ namespace AspNetCoreIdpInitiated
         }
 
         public IConfiguration Configuration { get; }
+
+        private static IHttpContextAccessor _httpContextAccessor;
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -35,6 +40,7 @@ namespace AspNetCoreIdpInitiated
                     opt.SPOptions.EntityId = new EntityId("https://sp.example.com");
                     opt.SPOptions.ReturnUrl = new Uri("/", UriKind.Relative);
 
+                    // Idp using RelayState as return url.
                     opt.IdentityProviders.Add(new IdentityProvider(
                         new EntityId("https://stubidp.sustainsys.com/d661ec76-b217-4f86-9149-50a5e0a651a4/Metadata"),
                         opt.SPOptions)
@@ -43,12 +49,41 @@ namespace AspNetCoreIdpInitiated
                         AllowUnsolicitedAuthnResponse = true,
                         RelayStateUsedAsReturnUrl = true
                     });
+
+                    // Idp using a custom query string param on the Acs URL.
+                    opt.IdentityProviders.Add(new IdentityProvider(
+                        new EntityId("https://stubidp.sustainsys.com/d661ec76-b217-4f86-9149-50a5e0a651a5/Metadata"),
+                        opt.SPOptions)
+                    {
+                        LoadMetadata = true,
+                        AllowUnsolicitedAuthnResponse = true,
+                    });
+
+                    opt.Notifications.AcsCommandResultCreated = AcsCommandResultCreated;
                 });
+
+            services.AddHttpContextAccessor();
+        }
+
+        private void AcsCommandResultCreated(CommandResult commandResult, Saml2Response saml2Response)
+        {
+            var httpContext = _httpContextAccessor.HttpContext;
+
+            var target = httpContext.Request.Query["target"].SingleOrDefault();
+
+            if(!string.IsNullOrEmpty(target))
+            {
+                commandResult.Location = new Uri(target, UriKind.Relative);
+            }
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IHttpContextAccessor httpContextAccessor)
         {
+            // The HttpContextAccessor is a singleton, so it can be assigned to a static variable. Not
+            // exactly elegant, but it works.
+            _httpContextAccessor = httpContextAccessor;
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
